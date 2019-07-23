010bbd4049fdf02b3ee3e196edcc66450098fa61
Andre <andfufe@gmail.com>
2019-07-23 18:18:19 -0300

    Primeiro commit, teste

---
+++ application/models/Aluno_model.php
@@ -0,0 +1,62 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Aluno_model extends CI_Controller{
+        var $table = 'alunos';
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->database();
+
+        }
+
+        ///////////////////////////////////////////CRUD////////////////////////////////////////
+
+        public function get_all_alunos()
+        {
+            $this->db->from('alunos');
+            $query=$this->db->get();
+            return $query->result();
+        }
+
+        public function get_by_id($codAluno)
+        {
+            $this->db->from($this->table);
+            $this->db->where('codAluno',$codAluno);
+            $query = $this->db->get();
+            return $query->row();
+        }
+
+        public function aluno_add($data)
+        {
+            $this->db->insert($this->table, $data);
+            return $this->db->insert_id();
+        }
+
+        public function aluno_update($where, $data)
+        {
+            $this->db->update($this->table, $data, $where);
+            return $this->db->affected_rows();
+        }
+
+        public function delete_by_id($codAluno)
+        {
+            $this->db->where('codAluno', $codAluno);
+            $this->db->delete($this->table);
+        }
+
+        /////////////////////////////////////////////////LOGIN////////////////////////////////////
+        function validate($email,$senha){
+            $this->db->where('email',$email);
+            $this->db->where('senha',$senha);
+            $result = $this->db->get('alunos',1);
+            return $result;
+        }
+
+    }
+
+
+
+
+
+
